2fe397ebf28c1e525dc129192c14e9635d40aee5
Livio <livio.brunner.lb1@roche.com>
2017-12-11 10:18:15 +0100

    Format balance

--- wallet-sum.js
+++ wallet-sum.js
@@ -7,6 +7,12 @@ const API_SECRET = keys.API_SECRET;
 
 const bfxRest = new BFX(API_KEY, API_SECRET, { version: 1 }).rest;
 
+let lastSum;
+
+/**
+ * Get a list of the users account wallet balances
+ * @returns {Promise<Object[]>} List of the users account wallet balances
+ */
 const loadWalletBalances = () => {
     return new Promise((resolve, reject) => {
         bfxRest.wallet_balances((err, res) => {
@@ -18,6 +24,12 @@ const loadWalletBalances = () => {
     });
 };
 
+/**
+ * Returns the given Cryptobalance in USD
+ * @param {string} type The Cryptocoins type e.g. IOT
+ * @param {Number} balance The balance as number to convert the price
+ * @returns {Promise<Number>} The given balance converted in USD
+ */
 const getBalanceInUsd = (type, balance) => {
     return new Promise((resolve, reject) => {
         bfxRest.ticker(`${type.toUpperCase()}USD`, (err, res) => {
@@ -29,6 +41,11 @@ const getBalanceInUsd = (type, balance) => {
     });
 };
 
+/**
+ * Returns the sum of the given balances
+ * @param {Object[]} balances The balances you want to get the USD sum of
+ * @returns {Promise<Number>} The sum of the given balances in USD
+ */
 const convertBalancesToUSDSum = balances => {
     return new Promise((resolve, reject) => {
         const cryptoBalances = balances.filter(balance => balance.currency !== 'usd' && balance.currency !== 'eur')
@@ -45,6 +62,10 @@ const convertBalancesToUSDSum = balances => {
     });
 };
 
+/**
+ * Get the current Balance of the set API Keys in USD
+ * @returns {Promise<Number>} The current Bitfinex balance in USD
+ */
 const getWalletSum = () => {
     return new Promise((resolve, reject) => {
         loadWalletBalances()
@@ -56,6 +77,36 @@ const getWalletSum = () => {
     });
 };
 
-getWalletSum()
-    .then(sum => console.log(sum))
-    .catch(console.err);
+/**
+ * Returns an ASCII icon, depending if the balance increased or decreased.
+ * @param {Number} sum The current sum
+ * @returns {String} The icon
+ */
+getIconFromSum = sum => {
+    if (!lastSum) {
+        return '';
+    }
+    return sum > lastSum ? '▲' : (sum < lastSum ? '▼' : '=');
+};
+
+/**
+ * Formats the sum by flooring it, and adding an icon
+ * @param {Number} sum The sum to format
+ * @returns {String} The formatted sum
+ */
+const formatSum = sum => {
+    const icon = getIconFromSum(sum);
+    return `${icon} ${sum.toFixed(2)}$`;
+};
+
+/**
+ * Runs the program
+ */
+const run = () => {
+    getWalletSum()
+        .then(sum => console.log(formatSum(sum)))
+        .catch(console.err);
+};
+
+
+run();
